6a85f14b3d28ba067c7b0fffec02e06e1031a372
Akib Rahman <akibrahman5200@gmail.com>
2025-02-20 22:49:41 +0600

    Trainer & Researcher model added

---
+++ TrainerAndResearcher.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const TrainerAndResearcherSchema = new mongoose.Schema({
+  name: { type: String, default: "" },
+  image: { type: String, default: "" },
+  designation: { type: String, default: "" },
+  description: { type: String, default: "" },
+  github: { type: String, default: "" },
+  researchGate: { type: String, default: "" },
+  linkedin: { type: String, default: "" },
+  createdAt: { type: String, default: "" },
+});
+
+export default mongoose.models.TrainerAndResearchers ||
+  mongoose.model("TrainerAndResearchers", TrainerAndResearcherSchema);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js in your iChainModels repo
 import CoFoundersAndFoundingBody from "./CoFoundersAndFoundingBody.js";
 import Researcher from "./Researcher.js";
+import TrainerAndResearcher from "./TrainerAndResearcher.js";
 
-export { CoFoundersAndFoundingBody, Researcher };
+export { CoFoundersAndFoundingBody, Researcher, TrainerAndResearcher };
